085af87fae6f34fe2e8de2f34708df8f2cec82af
Teja <lovlytejaa@gamil.com>
2022-10-28 13:35:29 +0530

    dot-group

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useMediaQuery from './hooks/useMediaQuery'
+import DotGroup from "./scenes/DotGroup";
 import Navbar from "./scenes/Navbar";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState('home')
   const isAboveMeduimScreens = useMediaQuery("min-width: 1060px")
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return (
     <div className="app bg-deep-blue">
-     <Navbar
-     selectedPage={selectedPage}
-     setSelectedPage={setSelectedPage}
-     />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        <DotGroup
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage} />
+      </div>
     </div>
   );
 }
--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -8,16 +8,16 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   const lowerCasePage = page.toLowerCase();
   return (
     <AnchorLink className={`${selectedPage === lowerCasePage ? "text-yellow" : ""} hover:text-yellow transition duration-500`}
-      onClick={() => setSelectedPage(lowerCasePage)}
+      href='#home' onClick={() => setSelectedPage(lowerCasePage)} 
     >
       {page}
     </AnchorLink>
   )
 }
-const Navbar = ({ selectedPage, setSelectedPage }) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false)
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
-
+  const navbarBackground = isTopOfPage ? " " : "bd-red"
   return (
     <nav className={`z-40 w-full fixed top-0 py-6`}>
       <div className='flex items-center justify-between mx-auto w-5/6'>
