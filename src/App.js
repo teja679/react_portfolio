815e38a8e2989057092ca78273ee03e64c322f13
Teja <lovlytejaa@gamil.com>
2022-10-28 13:12:22 +0530

    responsive navbar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import useMediaQuery from './hooks/useMediaQuery'
+import Navbar from "./scenes/Navbar";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState('home')
+  const isAboveMeduimScreens = useMediaQuery("min-width: 1060px")
+  return (
+    <div className="app bg-deep-blue">
+     <Navbar
+     selectedPage={selectedPage}
+     setSelectedPage={setSelectedPage}
+     />
+    </div>
+  );
+}
+
+export default App;
